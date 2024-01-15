ebef2357f4a0ba11908a2ef15a0122fa67dfb052
StasMakarchuk <49611600+StasMakarchuk@users.noreply.github.com>
2024-01-15 09:19:22 +0000

    Update _widget.py

--- napari_large_3D_vis_ch/_widget.py
+++ napari_large_3D_vis_ch/_widget.py
@@ -1,11 +1,3 @@
-"""
-This module is an example of a barebones QWidget plugin for napari
-
-It implements the Widget specification.
-see: https://napari.org/stable/plugins/guides.html?#widgets
-
-Replace code below according to your needs.
-"""
 import os
 from typing import TYPE_CHECKING
 
